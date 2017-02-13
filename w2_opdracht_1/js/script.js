c7ee00dcec6650c1f98ab89cd0da23ed0d43bf14
Chanel Mepschen <chanelmepschen@Chanels-MacBook-Pro.local>
2017-02-13 19:23:23 +0100

    Included the questions in the html. Using routie linked the hashes

--- w2_opdracht_1/js/script.js
+++ w2_opdracht_1/js/script.js
@@ -9,7 +9,64 @@ aja()
     })
     .go();
 
-console.log(JSON.parse(localStorage.getItem('data')));
+var data = JSON.parse(localStorage.getItem('data'));
+
+console.log(data.results);
+console.log(data.results.length);
+console.log(data.results.map(function(){return data.results.difficulty;}));
+
+(function () {
+    //Execute code in strict mode
+    'use strict';
+    
+    //Declare variables
+    var selectedElements = {
+        landingPage : document.getElementById('start'),
+        quiz : document.getElementById('quiz'),
+        quizList : document.querySelector('ol')
+    };
+    
+    var app = {
+        init: function(){
+            routes.init();
+        }
+    };
+    
+    var routes = {
+        init: function(){  
+            location.hash = '#start';
+            selectedElements.quiz.setAttribute('hidden', true);
+            
+            //window check if hash has changed, then put the hash in var x and execute sections.toggle(x). Source: Elton Goncalvez Gomez.
+            var urlHash = location.hash;
+            sections.toggle(urlHash);
+        }
+    };
+    
+    //Output all the questions
+    (function(){
+        for(var i=0; i < data.results.length; i++){
+        selectedElements.quizList.innerHTML += '<li id="question' + [i] + '">' + data.results[i].question + '</li>';
+        }
+    })();
+    
+    var sections = {
+        toggle: function(route){
+            routie('quiz', function(){
+                selectedElements.quiz.removeAttribute('hidden');
+                selectedElements.landingPage.setAttribute('hidden', true);
+            });
+            
+            routie('start', function(){
+                selectedElements.landingPage.removeAttribute('hidden');
+                selectedElements.quiz.setAttribute('hidden', true);
+            });
+        }
+    };
+    
+    app.init();
+    
+})();
 
 //Sources:
 //1 : Stackoverflow. (2010). Storing Objects in HTML5 localStorage. Source:

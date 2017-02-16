ca3a562e7376fc16967ccc1315e24091595d7df4
Chanel Mepschen <chanelmepschen@096.197.028.145.hva.nl>
2017-02-16 09:33:20 +0100

    refactored the code after feedback and made multiple routies

--- w2_opdracht_1/js/script.js
+++ w2_opdracht_1/js/script.js
@@ -1,69 +1,157 @@
 /*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
-aja()
-    .method('get')
-//API from https://opentdb.com/api_config.php
-    .url('https://opentdb.com/api.php?amount=20&category=14&difficulty=easy&type=multiple')
-    .type('json')
-    .on('200', function(data){
-        localStorage.setItem('data', JSON.stringify(data)); //source 1
-    })
-    .go();
-
-var data = JSON.parse(localStorage.getItem('data'));
-
-console.log(data.results);
-console.log(data.results.length);
-console.log(data.results.map(function(){return data.results.difficulty;}));
 
 (function () {
     //Execute code in strict mode
     'use strict';
     
-    //Declare variables
+    //Select elements in DOM and store them in selectedElements
     var selectedElements = {
         landingPage : document.getElementById('start'),
-        quiz : document.getElementById('quiz'),
-        quizList : document.querySelector('ol')
+        quiz : document.getElementById('categories'),
+        categoryList : document.querySelector('#categorylist'),
+        titleInput : document.querySelector('input[name="title"]'),
+        generateQuizButton : document.querySelector('button[type="submit"]'),
+        quizGenerator : document.querySelector('#quizgenerator'),
+        allQuestions : document.querySelector('.allquestions'),
+        givenTitle : document.querySelector('.mytitle')
     };
     
+    //Store data of api's in data.
+    var data = {
+        tv : function(){
+            aja()
+                .method('get')
+                //API from https://opentdb.com/api_config.php
+                .url('https://opentdb.com/api.php?amount=20&category=14&difficulty=easy&type=multiple')
+                .type('json')
+                .on('200', function(tv){ //If the API is succesful, store the data in tv
+                    localStorage.setItem('tv', JSON.stringify(tv)); //Source 1, put the data into local storage
+                    sections.render(tv, 'api'); //Execute function sections.render with the parameter that has the data from the api and a string to tell where the data is being loaded from
+                })
+                .go(); 
+        },
+        scienceNature : function(){
+            aja()
+                .method('get')
+                //API from https://opentdb.com/api_config.php
+                .url('https://opentdb.com/api.php?amount=20&category=17&difficulty=easy&type=multiple')
+                .type('json')
+                .on('200', function(scienceNature){
+                    localStorage.setItem('scienceNature', JSON.stringify(scienceNature)); //source 1
+                    sections.render(scienceNature, 'api');
+                })
+                .go();
+        },
+        sports : (function(){
+            aja()
+                .method('get')
+                //API from https://opentdb.com/api_config.php
+                .url('https://opentdb.com/api.php?amount=20&category=21&difficulty=easy&type=multiple')
+                .type('json')
+                .on('200', function(sports){
+                    localStorage.setItem('sports', JSON.stringify(sports)); //source 1
+                    sections.render(sports, 'api');
+                })
+                .go();
+        })(),
+        celebrities : (function(){
+            aja()
+                .method('get')
+                //API from https://opentdb.com/api_config.php
+                .url('https://opentdb.com/api.php?amount=20&category=26&difficulty=easy&type=multiple')
+                .type('json')
+                .on('200', function(celebrities){
+                    localStorage.setItem('celebrities', JSON.stringify(celebrities)); //source 1
+                    sections.render(celebrities, 'api');
+                })
+                .go();
+        })()
+    };
+    
+//    (function(){
+//        selectedElements.quizGenerator.hidden = true;
+//        
+//        selectedElements.generateQuizButton.addEventListener('click', function(event){
+//            event.preventDefault();
+//            
+//            var titleQuiz = {
+//                mytitle : selectedElements.titleInput.value
+//            };
+//            Transparency.render(selectedElements.quizGenerator, titleQuiz);
+//            Transparency.render(selectedElements.allQuestions, categoryNames);
+//            selectedElements.quizGenerator.hidden = false;
+//            selectedElements.landingPage.hidden = true;
+//        });
+//    })();
+//    
+    //Settings for starting app.
     var app = {
         init: function(){
             routes.init();
         }
     };
     
+    //Default settings, initiate toggle, if hash changes again initiate toggle 
     var routes = {
-        init: function(){  
-            location.hash = '#start';
-            selectedElements.quiz.setAttribute('hidden', true);
+        init: function(){ 
+            location.hash = '#home';
             
-            //window check if hash has changed, then put the hash in var x and execute sections.toggle(x). Source: Elton Goncalvez Gomez.
-            var urlHash = location.hash;
-            sections.toggle(urlHash);
+            routie({
+                'home': function() {
+                    console.log('home');
+                    sections.toggle('home');
+                },
+                'categories': function(){
+                    sections.toggle('categories');
+                },
+                'quizgenerator': function(){
+                    sections.toggle('quizgenerator');
+                },
+                'categories/:name': function(name) {
+                    localStorage.getItem(name) ? sections.render(JSON.parse(localStorage.getItem(name)), 'local') : data[name]();
+                }
+            });
         }
     };
-    
-    //Output all the questions
-    (function(){
-        for(var i=0; i < data.results.length; i++){
-        selectedElements.quizList.innerHTML += '<li id="question' + [i] + '">' + data.results[i].question + '</li>';
-        }
-    })();
     
     var sections = {
-        toggle: function(route){
-            routie('quiz', function(){
-                selectedElements.quiz.removeAttribute('hidden');
-                selectedElements.landingPage.setAttribute('hidden', true);
-            });
+        render: function(data, source) {
+            event.preventDefault();
             
-            routie('start', function(){
-                selectedElements.landingPage.removeAttribute('hidden');
-                selectedElements.quiz.setAttribute('hidden', true);
+            var categoryNames = [
+                Object.getOwnPropertyNames(data)[0], 
+                Object.getOwnPropertyNames(data)[1], 
+                Object.getOwnPropertyNames(data)[2], 
+                Object.getOwnPropertyNames(data)[3]
+            ];
+            
+            var directives = {
+                category : {
+                    href: function(params){
+                        return '#category/#' + this.value;
+                    },
+                    text : function(params){
+                        return this.value;
+                    }
+                }
+            };
+
+            Transparency.render(selectedElements.categoryList, categoryNames, directives);
+            // this.toggle()
+        },
+        toggle: function(route){
+            //Change all the other sections to hidden, except the active tab. Source: Shyanta Vleugel
+            var currentSection = document.querySelector('#' + route),
+                unactiveSections = document.querySelectorAll('section:not(#' + route + ')');
+            
+            unactiveSections.forEach(function(unactiveSections){
+                unactiveSections.hidden = true;
+                currentSection.hidden = false;
             });
         }
     };
     
+    //Initiate app
     app.init();
     
 })();
@@ -71,3 +159,15 @@ console.log(data.results.map(function(){return data.results.difficulty;}));
 //Sources:
 //1 : Stackoverflow. (2010). Storing Objects in HTML5 localStorage. Source:
 //http://stackoverflow.com/questions/2010892/storing-objects-in-html5-localstorage
+
+//Rubbish
+//
+    //Output all the questions
+//    (function(){
+//        for(var i=0; i < data.results.length; i++){
+//        selectedElements.categoryList.innerHTML += '<li id="question' + [i] + '"><a href="#question' + [i] + '">' + data.results[i].question + '</a></li><form><input type="radio" name="question' + [i] + '" value="' + data.results[i].incorrect_answers[0] + '" /><p>' + data.results[i].incorrect_answers[0] + '</p><input type="radio" name="question' + [i] + '" value="' + data.results[i].incorrect_answers[1] + '" /><p>' + data.results[i].incorrect_answers[1] + '</p><input type="radio" name="question' + [i] + '" value="' + data.results[i].incorrect_answers[2] + '" /><p>' + data.results[i].incorrect_answers[2] + '<input type="radio" name="question' + [i] + '" value="' + data.results[i].correct_answer + '" /><p>' + data.results[i].correct_answer + '</p></form>';
+//        }  
+//        selectedElements.answersInput = document.querySelectorAll('input[name^="question"]');
+//        selectedElements.answersParagraph = document.querySelectorAll('#categories p');
+//        
+//    })();
